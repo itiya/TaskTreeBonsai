4249ec91b1455087c43184443814a3a0320af129
sanyaitiya <fragment.vertex@gmail.com>
2016-09-13 22:23:41 +0900

    [add] mainとrenderでファイルを分けるように設定

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,10 +1,13 @@
 var path = require('path');
 
 module.exports = {
-    entry: "./src/main.ts",
+    entry: {
+        main: "./src/main/main.ts",
+        render: "./src/render/render.ts"
+    },
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: "bundle.js"
+        filename: "[name].bundle.js"
     },
     target: "electron",
     node: {
