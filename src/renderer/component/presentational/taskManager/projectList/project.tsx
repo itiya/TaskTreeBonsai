76863eaf946ab4e15becd37f64a3c01a349d5fd3
sanyaitiya <fragment.vertex@gmail.com>
2016-11-09 23:09:12 +0900

    [add] プロジェクトの編集を可能にする（stateへの反映はまだ）

--- src/renderer/component/presentational/taskManager/projectList/project.tsx
+++ src/renderer/component/presentational/taskManager/projectList/project.tsx
@@ -1,5 +1,8 @@
 /// <reference path="../../../../../../typings/index.d.ts" />
 import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import objectAssign = require('object-assign')
+
 var styles = require('./project.css');
 
 export interface Props{
@@ -9,7 +12,47 @@ export interface Props{
     onDeleteButtonClick: () => void;
 }
 
-export class Project extends React.Component<Props, any> {
+interface State {
+    isOnEdit: boolean;
+    editText: string;
+}
+
+export class Project extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            isOnEdit: false,
+            editText: props.name
+        }
+    }
+
+    startEdit() {
+        this.setState(objectAssign({}, this.state, {isOnEdit: true}))
+    }
+
+    changeText(target: HTMLInputElement) {
+        this.setState(objectAssign({}, this.state, {editText: target.value}))
+    }
+
+    endEdit() {
+        //this.props.onTaskEdit(this.props.task.id, this.state.editText)
+        this.setState(objectAssign({}, this.state, {isOnEdit: false}))
+    }
+
+    focusInput(input: HTMLInputElement) {
+        if(input){
+            ReactDOM.findDOMNode<HTMLInputElement>(input).focus()
+        }
+    }
+
+    projectName() {
+        if (this.state.isOnEdit) {
+            return <input type="text" ref={this.focusInput} onBlur={() => this.endEdit()} defaultValue={this.state.editText} onChange={(event) => this.changeText(event.target as HTMLInputElement)} /> 
+        } else {
+            return <div className={styles.text} onDoubleClick={() => this.startEdit()}>{this.props.name}</div>
+        }
+    }
+
     onDeleteButtonClick(event: React.MouseEvent) {
         if(confirm("プロジェクト「" + this.props.name + "」を削除しますか?")){
             this.props.onDeleteButtonClick()
@@ -26,7 +69,7 @@ export class Project extends React.Component<Props, any> {
         return (
             <li className={projectStyle} onClick={(event) => this.props.onClick()}>
                 <button className={styles.button} onClick={(event) =>this.onDeleteButtonClick(event)}>×</button>
-                {this.props.name}
+                {this.projectName()}
             </li>
         )
     }
